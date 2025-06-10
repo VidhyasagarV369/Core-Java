b22daa20b16791ea3858d3d9187728058b6760d3
VidhyasagarV369 <vidhyasagarv369@gmail.com>
2025-06-10 21:15:33 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class Loops {
+	public static void main (String[]args)
+	{
+	
+		for(int i=1; i<=100; i++)
+		{
+			if(i%5==0) {
+				continue;	
+			}
+		System.out.println(i );
+		}
+	}
+
+	}
+
